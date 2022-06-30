b6c95ec7d0a8d4ccfdc7cf7ab5c3864c05412aaf
Nikson <nikson@Admins-MacBook-Pro-27.local>
2022-06-30 23:30:18 +0530

    changes

--- workflow/tests/test_service.py
+++ workflow/tests/test_service.py
@@ -9,10 +9,12 @@ from Service.runner import json_reader, json_printer
 class Service(TestBase):
 
     def test_json_reader(self):
+        print(emoji.emojize("Start Testing for json_reader :snowflake:"))
         json_reader(os.getcwd() + "/tests/mock/mock_data.json")
         print(emoji.emojize('json_reader Test :thumbs_up:'))
 
     def test_json_printer(self):
+        print(emoji.emojize("Start Testing for json_printer :snowflake:"))
         data = json_reader(os.getcwd() + "/tests/mock/mock_data.json")
         json_printer(data)
         print(emoji.emojize('json_printer Test :thumbs_up:'))
